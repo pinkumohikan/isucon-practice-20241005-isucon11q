ffffbd7fb2dd847e19fae2b4a5e7a0a41d79991a
nishino-satoshi <nishino@tetemarche.co.jp>
2024-10-05 17:53:57 +0900

    update: isu_conditionの主キーを変更

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
-  PRIMARY KEY(`id`)
+  PRIMARY KEY(`jia_isu_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 ALTER TABLE `isu_condition` ADD INDEX idx_jia_isu_uuid_timestamp (`jia_isu_uuid`, `timestamp` DESC);
